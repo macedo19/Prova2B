ef2cc6e39ac017dd26d34fff8d28556ada080216
FelipeMacchiz <felipe.macchiz@gmail.com>
2021-11-24 22:02:32 -0300

    FormaPagamento Controller
    Exercício 3

--- API/Controllers/FormaPagamentoController.cs
+++ API/Controllers/FormaPagamentoController.cs
@@ -1,11 +1,41 @@
-// using System;
-// using System.Linq;
-// using API.Data;
-// using API.Models;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-// namespace API.Controllers
-// {
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/pagamento")]
+    public class FormaPagamentoController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public FormaPagamentoController(DataContext context)
+        {
+            _context = context;
+        }
 
-// }
+        //POST: api/pagamento/create
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create()
+        {
+            _context.FormaPagamentos.AddRange(new FormaPagamento[]
+                {
+                    new FormaPagamento { Id = 1, Descricao = "Cartão de crédito", Bandeira = "Mastercard", ChavePix = "" },
+                    new FormaPagamento { Id = 1, Descricao = "Cartão de débito", Bandeira = "Visa", ChavePix = "" },
+                    new FormaPagamento { Id = 1, Descricao = "Pix", Bandeira = "", ChavePix = "456721936258" },
+                }
+            );
+            _context.SaveChanges();
+            return Ok(new { message = "Dados inicializados com sucesso!" });
+        }
+
+        //GET: api/pagamento/list
+        [HttpGet]
+        [Route("list")]
+        public IActionResult List() => Ok(_context.FormaPagamentos.ToList());
+    }
+}
