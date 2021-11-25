8f2b55fe44808f746ec1fc9073a85a4cad7e7220
Ana Vargas <61504223+macedo19@users.noreply.github.com>
2021-11-24 21:09:53 -0300

    Alteração 1
    Realizado a alteração da Model e adição de Controller

---
+++ API/Controllers/FormaPagamentoController.cs
@@ -0,0 +1,11 @@
+// using System;
+// using System.Linq;
+// using API.Data;
+// using API.Models;
+// using Microsoft.AspNetCore.Mvc;
+// using Microsoft.EntityFrameworkCore;
+
+// namespace API.Controllers
+// {
+
+// }
---
+++ API/Models/FormaPagamento.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace API.Models
+{
+    public class FormaPagamento
+    {
+        public FormaPagamento() => CriadoEm = DateTime.Now;
+        public int Id { get; set; }
+        public string Bandeira { get; set; }
+        public string Descricao { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
