49f578045cdf825e1f51d9d7aebd7e6ce5e740e8
Ana Vargas <61504223+macedo19@users.noreply.github.com>
2021-11-24 21:26:32 -0300

    Inicio do Front
    Criacao do Venda de produto

--- FRONT/src/app/app-routing.module.ts
+++ FRONT/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { CarrinhoComponent } from "./components/views/home/carrinho/carrinho.component";
 import { IndexComponent } from "./components/views/home/index/index.component";
 import { CadastrarProdutoComponent } from "./components/views/produto/cadastrar-produto/cadastrar-produto.component";
+import { CadastrarVendaComponent } from "./components/views/produto/cadastrar-venda/cadastrar-venda.component";
 import { ListarProdutoComponent } from "./components/views/produto/listar-produto/listar-produto.component";
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
     {
         path: "produto/cadastrar",
         component: CadastrarProdutoComponent,
+    },
+    {
+        path: "produto/cadastrar-venda",
+        component: CadastrarVendaComponent,
     },
 ];
 
